7df9c8cb50a6fcab82b8ff9b22052e946f4098fb
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-03 17:09:04 -0700

    Se agrego el program.cs pasado

--- ListasDoblementeLigadasCirculares/Program.cs
+++ ListasDoblementeLigadasCirculares/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListasDoblementeLigadasCirculares;
+
+Lista listas = new Lista();
+
+Console.WriteLine("Mostrando elementos de la lista");
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Tres");
+listas.AgregarNodo("Cuatro");
+Console.WriteLine(listas.RecorrerLista());
+Console.WriteLine("---------------------------------------------------------");
+
+Console.WriteLine("Buscando siguiente a Cuatro");
+Nodo nodoPosteriorAtres = listas.Buscar("Cuatro");
+Console.WriteLine(nodoPosteriorAtres.Enlace.Enlace.Valor);
+Console.WriteLine("--------------------------------------------------------");
+
+Console.WriteLine("Borrar Uno");
+listas.BorrarNodo("Uno");
+Console.WriteLine(listas.RecorrerLista());
